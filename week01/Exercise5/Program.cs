173ebd49fbec0db2a489318952c42eac9e7c43ca
gbemi001 <oyoloye@byupathway.edu>
2026-01-10 15:59:35 +0100

    First commit for exercise 5.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcomeMessage();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squaredNumber);
+    }
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Program.");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string UserName = Console.ReadLine();
+        return UserName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        int UserNumber = int.Parse(Console.ReadLine());
+        return UserNumber;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(string UserName, int square)
+    {
+        Console.WriteLine($"{UserName}, the square of your number is {square} ");
     }
 }
