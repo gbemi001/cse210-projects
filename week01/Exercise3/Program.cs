0ea18e67d938b4edd466a68b1bc22c82cbe864b3
gbemi001 <oyoloye@byupathway.edu>
2026-01-10 12:09:30 +0100

    exercise 3 first try!

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int MagicNumber = randomGenerator.Next(1, 101);
+        string response;
+        do
+        {
+            Console.WriteLine("What is your guess? ");
+            string GuessInput = Console.ReadLine();
+            int GuessNumber = int.Parse(GuessInput);
+
+            if (GuessNumber < MagicNumber)
+            {
+                Console.WriteLine($"Higher");
+                response = "no";
+            }
+            else if (GuessNumber > MagicNumber)
+            {
+                Console.WriteLine($"Lower");
+                response = "no";
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it!");
+                response = "yes";
+            }
+                
+            } while (response == "no");
+
     }
 }
