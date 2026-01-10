f58b616006b46d02d50182fac3852c0ba25df0c3
gbemi001 <oyoloye@byupathway.edu>
2026-01-10 14:19:11 +0100

    exercise 4, first dip!

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List <int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int UserNumber;
+        do
+        {
+            Console.WriteLine("Enter number: ");
+            string UserInput = Console.ReadLine();
+            UserNumber = int.Parse(UserInput);
+
+            if (UserNumber != 0)
+            {
+                numbers.Add(UserNumber);
+            } 
+        } while (UserNumber != 0);
+
+        int sum = numbers.Sum();
+        float average = (float)sum / numbers.Count;
+        int max = numbers.Max();
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
