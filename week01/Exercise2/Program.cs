ddcfccc278c0134c23e9081bfee059e864067691
gbemi001 <oyoloye@byupathway.edu>
2026-01-10 11:00:07 +0100

    second exercise commit

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        string letter = "";
+        Console.WriteLine("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int gradePercent = int.Parse(userInput);
+
+        if (gradePercent >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";;
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (gradePercent >= 70)
+        {
+            Console.WriteLine($"Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine($"Try again! You can do better next time.");
+        }
     }
 }
