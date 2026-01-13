c6cb4aea11021c297c15e059e7079f311f91349b
gbemi001 <oyoloye@byupathway.edu>
2026-01-13 22:30:23 +0100

    fresh Abstraction learning

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,12 @@
+public class Job 
+{
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    public void DisplayJobDetails(string title, string company, int startYear, int endYear)
+    {
+        Console.WriteLine($"{title} ({company}) {startYear}-{endYear}");
+    }
+}
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Developer";
+        job1._company = "Tech Corp";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+        job1.DisplayJobDetails(job1._jobTitle, job1._company, job1._startYear, job1._endYear);
+
+        Job job2 = new Job();
+        job2._jobTitle = "AI Engineer";
+        job2._company = "Microsoft";
+        job2._startYear = 2023;
+        job2._endYear = 2025;
+        job2.DisplayJobDetails(job2._jobTitle, job2._company, job2._startYear, job2._endYear);
+
+        Resume myResume = new Resume();
+        myResume._name = "Oluwagbemi Yoloye";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.DisplayResume();
     }
 }
